616e8d2a29abf86544a8cfee05b7b9dcee8677aa
huypl53 <phamlehuy53@gmail.com>
2024-06-02 20:34:39 +0700

    feat: implement activation function sigmoid, relu, elu

---
+++ exercises/module01_week01_240601/activation.py
@@ -0,0 +1,69 @@
+import math
+import os
+import sys
+from typing import Any, Callable, Dict
+
+current_path = sys.path[0]
+aio_path = current_path.rsplit(os.path.sep, 2)[0]
+sys.path.append(aio_path)
+
+from aio24.validator.base import InputTypeValidator
+
+
+@InputTypeValidator(float)
+def get_sigmoid(x: float) -> float:
+    return 1 / (1 + math.e ** (-x))
+
+
+@InputTypeValidator(float)
+def get_relu(x: float) -> float:
+    if x <= 0:
+        return 0
+    return x
+
+
+@InputTypeValidator(float)
+def get_elu(x: float, alpha=0.01) -> float:
+    if x <= 0:
+        return alpha ** (math.e**x - 1)
+    return x
+
+
+def is_number(x: Any) -> bool:
+    try:
+        float(x)
+        return True
+    except ValueError:
+        return False
+
+
+VALID_ACTIVATIONS: Dict[str, Callable] = {
+    "sigmoid": get_sigmoid,
+    "relu": get_relu,
+    "elu": get_elu,
+}
+
+
+def is_valid_activation(func_name: str) -> bool:
+    if func_name in VALID_ACTIVATIONS:
+        return True
+    return False
+
+
+def print_activated_feature():
+    x = input("Input x = ")
+    if not is_number(x):
+        raise ValueError(f"{x} has to be number")
+    x = float(x)
+    activation_func = input(
+        f"Input activation Function ({'|'.join(VALID_ACTIVATIONS)}): "
+    )
+    if not is_valid_activation(activation_func):
+        raise ValueError(f"{activation_func} function is not supported")
+
+    y = VALID_ACTIVATIONS[activation_func](x)
+    print(f"{activation_func}: f({x}) = {y}")
+
+
+if __name__ == "__main__":
+    print_activated_feature()
