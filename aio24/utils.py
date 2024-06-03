6830acfe4708baa223b37baf7011a440c701920b
huypl53 <phamlehuy52@gmail.com>
2024-06-03 16:26:39 +0700

    feat: aio24 adds function read from stdin

--- aio24/utils.py
+++ aio24/utils.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Type
 
 
 def is_number(x: Any) -> bool:
@@ -7,3 +7,11 @@ def is_number(x: Any) -> bool:
         return True
     except ValueError:
         return False
+
+
+def get_std_input(msg: str, input_type: Type) -> Any:
+    x = input(msg)
+    try:
+        return input_type(x)
+    except Exception:
+        raise ValueError(f"{x} must be {input_type}")
